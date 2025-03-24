fa65bada2cc9f0aef13723f3c27854f0c1beb9d0
Mohd Hazmi <PC@DESKTOP-NMF44U6>
2025-03-24 09:37:05 +0800

    Created Models, edited Program for DBContext

---
+++ GDEXTodoApi/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GDEXTodoApi.Models
+{
+    public class TodoContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ GDEXTodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace GDEXTodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Status { get; set; }
+    }
+}

f29bede74f675ba0635aa78368f251baa3bfde34
Mohd Hazmi <PC@DESKTOP-NMF44U6>
2025-03-25 11:42:54 +0800

    Add initializers to populate inmemory db on launch

---
+++ GDEXTodoApi/Models/DbInitializer.cs
@@ -0,0 +1,23 @@
+﻿namespace GDEXTodoApi.Models
+{
+    public static class DbInitializer
+    {
+        public static void SeedItems(TodoContext context)
+        {
+            if (context.TodoItems.Any())
+            {
+                return;
+            }
+
+            var items = new List<TodoItem>
+            {
+                new TodoItem { Id = 1, Name = "Hazmi", Description = "Test Todo 1", DueDate = DateTime.Now, Status = "In Progress"},
+                new TodoItem { Id = 2, Name = "Johnny", Description = "Test Todo 2", DueDate = DateTime.Now, Status = "Completed"},
+                new TodoItem { Id = 3, Name = "Shamsul", Description = "Test Todo 3", DueDate = DateTime.Now, Status = "Overdue"}
+            };
+
+            context.TodoItems.AddRange(items);
+            context.SaveChanges();
+        }
+    }
+}
--- GDEXTodoApi/Program.cs
+++ GDEXTodoApi/Program.cs
@@ -16,6 +16,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using(var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<TodoContext>();
+    DbInitializer.SeedItems(context);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
